8bc4c8a9502f53934b3ed5d68c9cc36e917a73ca
Usman Ali <60957279+uakk101@users.noreply.github.com>
2023-05-30 18:29:53 +0500

    Uploading to github

---
+++ src/pages/homepage.jsx
@@ -0,0 +1,212 @@
+import React, { useState, useEffect } from "react";
+
+import { faMailBulk } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faTwitter,
+	faGithub,
+	faStackOverflow,
+	faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+
+import Logo from "../components/common/logo";
+import Footer from "../components/common/footer";
+import NavBar from "../components/common/navBar";
+import Article from "../components/homepage/article";
+import Works from "../components/homepage/works";
+import AllProjects from "../components/projects/allProjects";
+
+import INFO from "../data/user";
+import myArticles from "../data/articles";
+
+import "./styles/homepage.css";
+
+const Homepage = () => {
+	const [stayLogo, setStayLogo] = useState(false);
+	const [logoSize, setLogoSize] = useState(80);
+	const [oldLogoSize, setOldLogoSize] = useState(80);
+
+	useEffect(() => {
+		document.title = INFO.main.title;
+		window.scrollTo(0, 0);
+	}, []);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			let scroll = Math.round(window.pageYOffset, 2);
+
+			let newLogoSize = 80 - (scroll * 4) / 10;
+
+			if (newLogoSize < oldLogoSize) {
+				if (newLogoSize > 40) {
+					setLogoSize(newLogoSize);
+					setOldLogoSize(newLogoSize);
+					setStayLogo(false);
+				} else {
+					setStayLogo(true);
+				}
+			} else {
+				setLogoSize(newLogoSize);
+				setStayLogo(false);
+			}
+		};
+
+		window.addEventListener("scroll", handleScroll);
+		return () => window.removeEventListener("scroll", handleScroll);
+	}, [logoSize, oldLogoSize]);
+
+	const logoStyle = {
+		display: "flex",
+		position: stayLogo ? "fixed" : "relative",
+		top: stayLogo ? "3vh" : "auto",
+		zIndex: 999,
+		border: stayLogo ? "1px solid white" : "none",
+		borderRadius: stayLogo ? "50%" : "none",
+		boxShadow: stayLogo ? "0px 4px 10px rgba(0, 0, 0, 0.25)" : "none",
+	};
+
+	return (
+		<React.Fragment>
+			<div className="page-content">
+				<NavBar active="home" />
+				<div className="content-wrapper">
+					<div className="homepage-logo-container">
+						<div style={logoStyle}>
+							<Logo width={logoSize} link={false} />
+						</div>
+					</div>
+
+					<div className="homepage-container">
+						<div className="homepage-first-area">
+							<div className="homepage-first-area-left-side">
+								<div className="title homepage-title">
+									{INFO.homepage.title}
+								</div>
+
+								<div className="subtitle homepage-subtitle">
+									{INFO.homepage.description}
+								</div>
+							</div>
+
+							<div className="homepage-first-area-right-side">
+								<div className="homepage-image-container">
+									<div className="homepage-image-wrapper">
+										<img
+											src="homepage.jpg"
+											alt="about"
+											className="homepage-image"
+										/>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						{/* Home Page Social Medai Links */}
+
+						<div className="homepage-socials">
+												
+
+							<a
+								href={INFO.socials.twitter}
+								target="_blank"
+								rel="noreferrer"
+								className="back"
+							>
+								
+									<FontAwesomeIcon
+										icon={faTwitter}
+										className="front"
+										
+									/>
+								
+								
+							</a>
+							<a
+								href={INFO.socials.github}
+								target="_blank"
+								rel="noreferrer"
+								className="back"
+							>
+								<FontAwesomeIcon
+									icon={faGithub}
+									className="front"
+								/>
+							</a>
+							<a
+								href={INFO.socials.stackoverflow}
+								target="_blank"
+								rel="noreferrer"
+								className="back"
+							>
+								<FontAwesomeIcon
+									icon={faStackOverflow}
+									className="front"
+								/>
+							</a>
+							<a
+								href={INFO.socials.instagram}
+								target="_blank"
+								rel="noreferrer"
+								className="back"
+							>
+								<FontAwesomeIcon
+									icon={faInstagram}
+									className="front"
+								/>
+							</a>
+							<a
+								href={`mailto:${INFO.main.email}`}
+								target="_blank"
+								rel="noreferrer"
+								className="back"
+							>
+								<FontAwesomeIcon
+									icon={faMailBulk}
+									className="front"
+								/>
+							</a>
+
+	
+						</div>
+
+						{/* Home Page social Links Finished */}
+
+
+						<div className="homepage-projects">
+							<AllProjects />
+						</div>
+
+						<div className="homepage-after-title">
+							<div className="homepage-articles">
+								{myArticles.map((article, index) => (
+									<div
+										// className="homepage-article"
+										key={(index + 1).toString()}
+									>
+										<Article
+											key={(index + 1).toString()}
+											date={article().date}
+											title={article().title}
+											description={article().description}
+											link={"/article/" + (index + 1)}
+										/>
+									</div>
+								))}
+							</div>
+
+							<div className="homepage-works">
+								<Works />
+							</div>
+						</div>
+
+						<div className="page-footer">
+							<Footer />
+						</div>
+					</div>
+				</div>
+			</div>
+		</React.Fragment>
+	);
+};
+
+export default Homepage;
